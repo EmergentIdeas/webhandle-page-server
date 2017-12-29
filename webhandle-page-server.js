7247dbe2d0bac6e13a367816a065dbc44b6e0306
Dan Kolz <dan@emergentideas.com>
2017-12-29 10:29:55 -0600

    page serving with the ability to add handlers after the page meta is loaded but before the age renders

--- webhandle-page-server.js
+++ webhandle-page-server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const filog = require('filter-log')
 const fs = require('fs')
+const commingle = require('commingle')
 
 let log = filog('webhandle:page-server')
 
@@ -13,40 +14,66 @@ const allowedPath = function(path) {
 }
 
 let createPageServer = function(sourceDirectory) {
-	
 	let server = function(req, res, next) {
 		if(!allowedPath(req.path)) {
 			return next()
 		}
 		
 		let fullPath = path.join(sourceDirectory, req.path)
-		let parsedPath = path.parse(fullPath)
-		fs.readdir(parsedPath.dir, function(err, items) {
-			if(err) {
-				log.debug('No page found for: ' + req.path)
-				return next()
+		fs.stat(fullPath, function(err, data) {
+			let isDirectory = data && data.isDirectory()
+			
+			let parsedPath = path.parse(fullPath)
+			let containingPath = isDirectory ? fullPath : parsedPath.dir
+			
+			if(containingPath.toString().indexOf(sourceDirectory.toString()) != 0) {
+				log.error("Attacking detected for path: " + req.path)
+				return res.setStatus(404)
 			}
 			
-			// serve an exact file match
-			for(let item of items) {
-				if((parsedPath.name + '.tri') === item) {
-					fs.readFile(parsedPath.dir + '/' + parsedPath.name + '.json', function(err, data) {
-						if(!err) {
-							log.debug('Found page meta information for: ' + req.path)
-							res.locals.page = JSON.parse(data.toString())
-						}
-						log.debug('Serving page for: ' + req.path)
-						res.render(path.dirname(req.path) + '/' + parsedPath.name)
-					})
-					return
+			fs.readdir(containingPath, function(err, items) {
+				if(err) {
+					log.debug('No page found for: ' + req.path)
+					return next()
 				}
-			}
+
+				for(let currentName of ( isDirectory ? server.indexNames : [parsedPath.name])) {
+					for(let item of items) {
+						if((currentName + '.tri') === item) {
+							log.debug('Serving page for: ' + req.path)
+							fs.readFile(containingPath + '/' + currentName + '.json', function(err, data) {
+								if(!err) {
+									log.debug('Found page meta information for: ' + req.path)
+									try {
+										res.locals.page = JSON.parse(data.toString())
+									}
+									catch(e) {}
+								}
+								res.locals.page = res.locals.page || {}
+								
+								
+								commingle([...server.preRun])(req, res, () => {
+									res.render((isDirectory ? req.path : path.dirname(req.path)) + '/' + currentName)
+								})
+							})
+							return
+						}
+					}
+				}
+				
+				return next()
+			})
 			
-			return next()
 		})
 	}
 	
+	server.preRun = []
+	server.indexNames = ['index']
+	
+	
 	return server
 }
+
+
 
 module.exports = createPageServer
