5271bc13a8e23e0f1c89f6e72e8fbada52a76930
Dan Kolz <dan@emergentideas.com>
2019-05-16 23:43:11 -0500

    makes finding the info for pages callable by other

--- webhandle-page-server.js
+++ webhandle-page-server.js
@@ -13,13 +13,13 @@ const allowedPath = function(path) {
 	return true
 }
 
+
+
+
 let createPageServer = function(sourceDirectory) {
-	let server = function(req, res, next) {
-		if(!allowedPath(req.path)) {
-			return next()
-		}
-		
-		let fullPath = path.join(sourceDirectory, decodeURI(req.path))
+	
+	let findPageInfo = function(reqpath, callback) {
+		let fullPath = path.join(sourceDirectory, decodeURI(reqpath))
 		fs.stat(fullPath, function(err, data) {
 			let isDirectory = data && data.isDirectory()
 			
@@ -27,56 +27,91 @@ let createPageServer = function(sourceDirectory) {
 			let containingPath = isDirectory ? fullPath : parsedPath.dir
 			
 			if(containingPath.toString().indexOf(sourceDirectory.toString()) != 0) {
-				log.error("Attacking detected for path: " + req.path)
+				log.error("Attacking detected for path: " + reqpath)
 				return res.setStatus(404)
 			}
 			
 			fs.readdir(containingPath, function(err, items) {
 				if(err) {
-					log.debug('No page found for: ' + req.path)
+					log.debug('No page found for: ' + reqpath)
 					return next()
 				}
 
 				for(let currentName of ( isDirectory ? server.indexNames : [parsedPath.name])) {
 					for(let item of items) {
 						if((currentName + '.tri') === item) {
-							log.debug({
-								message: 'Serving page for: ' + req.path,
-								path: req.path,
-								method: req.method,
-								hostname: req.hostname,
-								ip: req.ip,
-								protocol: req.protocol,
-								userAgent: req.headers['user-agent'],
-								fileName: path.join(containingPath, item),
-								type: 'page-view'
-							})
 							fs.readFile(containingPath + '/' + currentName + '.json', function(err, data) {
+								let info = {
+									currentName: currentName,
+									containingPath: containingPath,
+									item: item,
+									isDirectory: isDirectory,
+									templatePath: (isDirectory ? decodeURI(reqpath) : path.dirname(decodeURI(reqpath))) + '/' + currentName
+								}
 								if(!err) {
-									log.debug('Found page meta information for: ' + req.path)
+									log.debug('Found page meta information for: ' + reqpath)
 									try {
-										res.locals.page = JSON.parse(data.toString())
+										info.pageInfo = JSON.parse(data.toString())
 									}
 									catch(e) {}
 								}
-								res.locals.page = res.locals.page || {}
 								
-								
-								commingle([...server.preRun])(req, res, () => {
-									res.set('Content-Type', 'text/html; charset=UTF-8')
-									res.render((isDirectory ? decodeURI(req.path) : path.dirname(decodeURI(req.path))) + '/' + currentName)
-								})
+								if(callback) {
+									callback(null, info)
+								}
+								return
 							})
 							return
 						}
 					}
 				}
 				
-				return next()
+				if(callback) {
+					callback()
+				}
+				return
 			})
 			
 		})
+		
 	}
+	
+	
+	let server = function(req, res, next) {
+		if(!allowedPath(req.path)) {
+			return next()
+		}
+		
+		findPageInfo(req.path, (err, info) => {
+			if(!info) {
+				return next()
+			}
+			
+			res.locals.page = info.pageInfo || {}
+			
+			
+			
+			
+			commingle([...server.preRun])(req, res, () => {
+				log.debug({
+					message: 'Serving page for: ' + req.path,
+					path: req.path,
+					method: req.method,
+					hostname: req.hostname,
+					ip: req.ip,
+					protocol: req.protocol,
+					userAgent: req.headers['user-agent'],
+					fileName: path.join(info.containingPath, info.item),
+					type: 'page-view'
+				})
+
+				res.set('Content-Type', 'text/html; charset=UTF-8')
+				res.render(info.templatePath)
+			})
+		})
+		
+	}
+	server.findPageInfo = findPageInfo
 	
 	server.preRun = []
 	server.indexNames = ['index']
